397ab4b977a135e48d876a0e9b8721d6b866b697
Noah  Klem <klem.noah@gmail.com>
2021-05-27 15:36:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array.each do |number|
   squared_number = number * number
   new_numbers = []
-  new_numbers << squared_number
+  new_numbers.push(squared_number)
   puts new_numbers
   end
 end
